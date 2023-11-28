9cfce6cb5cbed55ffe796cee869a6b72b891eb71
Pedro Ulisses <pedroupcm@gmail.com>
2023-11-28 15:18:41 -0300

    Baitolagens

--- main.py
+++ main.py
@@ -1,11 +1,48 @@
 from classes.casetests.casetestsgraph import CaseTestsGraphs
+import time
+import os
+
+def limpar_tela():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
 pathFile = "db/USA-road-d.NY.gr.txt"
 
-print("Selecione uma opção :")
-print("1 - Grafo não Direcionado        2 - Digrafo        3 - Sair")
-escolhaGrafo = str(input())
-
-if int(escolhaGrafo) == 1:
-    casetestsgraphs = CaseTestsGraphs(pathFile)
-elif int(escolhaGrafo) == 2:
-    casetestsgraphs = CaseTestsGraphs(pathFile)
+while True:
+    print("Selecione uma opção :")
+    print("1 - Grafo não Direcionado        2 - Digrafo        3 - Sair")
+    escolha = str(input())
+    
+    if int(escolha) == 1:
+        casetestsgraphs = CaseTestsGraphs(pathFile)
+        print("Pressione ENTER para continuar")
+        pressioneEnter = str(input())
+        limpar_tela()
+        
+    elif int(escolha) == 2:
+        casetestsgraphs = CaseTestsGraphs(pathFile)
+        print("Pressione ENTER para continuar")
+        pressioneEnter = str(input())
+        limpar_tela()
+        
+    elif int(escolha) == 3:
+        print("Saindo ")
+        time.sleep(1)
+        print(".")
+        time.sleep(1)
+        print("..")
+        time.sleep(1)
+        print("...")
+        time.sleep(1)
+        print("Até a proxima")
+        time.sleep(2)
+        limpar_tela()
+        break
+    
+    else:
+        print("Selecione uma opção válida")
+        print("Pressione ENTER para continuar")
+        pressioneEnter = str(input())
+        limpar_tela()
